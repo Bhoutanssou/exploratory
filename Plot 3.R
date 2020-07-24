bec2b52abaf20a9f702c278bf0f4d26fe4f8efcb
Bhoutanssou <31318313+Bhoutanssou@users.noreply.github.com>
2020-07-24 17:14:04 +0200

    Create Plot 3.R

---
+++ Plot 3.R
@@ -0,0 +1,36 @@
+## Load the table 
+
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+  
+## Filter data : Feb. 1, 2007 to Feb. 2, 2007
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+  
+## Remove observations which are incomplete
+t <- t[complete.cases(t),]
+
+## Combine Date and Time & remove it to replace them by a merge one
+dateTime <- paste(t$Date, t$Time)
+dateTime <- setNames(dateTime, "DateTime")
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+t <- cbind(dateTime, t)
+  
+## Format dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+
+
+## Create the histograms
+
+  
+   # Plot 3
+   
+     with(t, {
+    plot(Sub_metering_1~dateTime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~dateTime,col='Red')
+    lines(Sub_metering_3~dateTime,col='Blue')
+  })
+  legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
